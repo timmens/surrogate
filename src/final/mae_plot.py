7cffa533c703236340db56bb59f107f31e0175a7
timmens <tim@mensinger96.com>
2020-09-05 14:10:32 +0200

    Update MAE plot.

--- src/final/mae_plot.py
+++ src/final/mae_plot.py
@@ -1,54 +1,40 @@
-from pathlib import Path
-
-import click
-import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-from bld.project_paths import project_paths_join as ppj
+from src.config import BLD
 
 
-def translations():
-    kwarg_translation = {
-        "f-degree-1-p-None": "Linear",
-        "f-degree-2-p-None": "Quadratic",
-        "f-layers-88-88-88-88-88-n-epochs-200-n-batch-size-128-p-None": "Large",
-        "f-layers-88-88-88-n-epochs-200-n-batch-size-128-p-None": "Medium",
-        "f-layers-88-88-n-epochs-200-n-batch-size-128-p-None": "Small",
-        "f-learning-rate-005-p-None": "SlowLearner",
-        "f-learning-rate-01-p-None": "FastLearner",
-    }
-    return kwarg_translation
+def load_losses():
+    """Load losses from bld folder for each project specification."""
+    path = BLD / "evaluations"
+    project_names = [p.stem for p in path.glob("*")]
+    losses = {name: pd.read_csv(path / name / "losses.csv") for name in project_names}
+    return losses
 
 
-@click.command()
-@click.argument("model", type=str)
-def main(model):
-    load_path = Path(ppj("OUT_FINAL")) / f"{model}-losses_tidy.csv"
-    df = pd.read_csv(load_path)
+def mae_plot(losses):
+    """Make plot from losses data frame."""
+    plot = sns.lineplot(x="n_obs", y="mae", hue="model", data=losses)
+    fig = plot.get_figure()
+    ax = fig.get_axes()[0]
+    fig.set_size_inches(11.7, 8.27)
+    ax.legend(loc="upper right")
+    return fig
 
-    kwarg_translations = translations()
-    df = df.replace(kwarg_translations)
 
-    methods = list(set(df.method))
-    sns.set(font_scale=1.5, style="ticks")
+def save_fig(path, fig):
+    """Save figure to bld path."""
+    fig.savefig(path / "mae_plot.png", bbox_inches="tight")
 
-    fig, axs = plt.subplots(1, len(methods), sharex=True, sharey=True)
-    fig.set_size_inches(11.7, 8.27)  # A4 paper
-    fig.suptitle("Model performance given varying training data size")
 
-    for ax, method in zip(axs, methods):
-        ax = sns.lineplot(
-            x="n_obs", y="mae", hue="kwargs", data=df.query("method==@method"), ax=ax
-        )
-        ax.set(xscale="log")
-        ax.set_ylim(0, df["mae"].max())
-        ax.set_xlim(200, 22000)
-        ax.legend(title=method, loc="upper center", bbox_to_anchor=(0.5, 0.3))
-
-    fig_path = Path(ppj("OUT_FIGURES")) / f"{model}-mae_plot.png"
-    fig.savefig(fig_path, bbox_inches="tight")
+def main():
+    losses_dict = load_losses()
+    for project_name, losses in losses_dict.items():
+        fig = mae_plot(losses)
+        path = BLD / "figures" / project_name
+        path.mkdir(parents=True, exist_ok=True)
+        save_fig(path, fig)
 
 
 if __name__ == "__main__":
-    main()  # pylint: disable-no-value
+    main()
